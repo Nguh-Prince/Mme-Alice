465fcf0dcb7a00ab31f83e52773857a02686bda9
prince <nguhprince1@gmail.com>
2022-06-20 08:41:47 +0100

    Created superuser and registered some admin models

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+admin.site.register(models.Candidate)
+admin.site.register(models.IDCard)
+admin.site.register(models.Vote)
+admin.site.register(models.VoteID)
